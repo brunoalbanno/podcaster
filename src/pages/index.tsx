18e7a3a574c640809da230c2f4e1e8353b340e59
Your NameBruno <brunomalbano@gmail.com>
2021-04-22 03:34:58 -0300

    Troca do método fetch para axios no consumo da api do json-server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,35 @@
-import { useEffect } from "react"
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   console.log(props.episodes)
 
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
 
   return {
     props: {
