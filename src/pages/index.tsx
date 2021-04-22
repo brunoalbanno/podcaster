e51531738a825110e04e254d1e9a966a18e06c5a
Your NameBruno <brunomalbano@gmail.com>
2021-04-22 03:56:26 -0300

    Formatação dos dados ao ser requisitados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
 type Episode = {
   id: string;
   title: string;
+  thumbnail: string;
   members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
 }
 
 type HomeProps = {
@@ -12,7 +21,7 @@ type HomeProps = {
 }
 
 export default function Home(props: HomeProps) {
-  console.log(props.episodes)
+  
 
   return (
     <div>
@@ -31,9 +40,23 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   });
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    };
+  });
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   }
